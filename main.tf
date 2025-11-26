8cbb1d403fedaebb9f0390cad88965854cee272e
Uttej Ghanta <uttejmulticloud@gmail.com>
2025-11-26 11:39:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 resource "aws_instance" "one" {
- count                  = 1
- ami                    = "ami-0ecb62995f68bb549"
-  instance_type          = "t3.micro"
+  count         = 1
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+
   tags = {
     Name = "Prod"
-    ENV = "Prod"
+    Env  = "Prod"  # must match Sentinel
   }
 }
-
 
 
 
