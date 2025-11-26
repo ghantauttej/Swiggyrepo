93efda588a625555d00b13bf82aae4756c3511e2
Uttej Ghanta <uttejmulticloud@gmail.com>
2025-11-26 10:34:32 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "one" {
+ ami                    = "ami-0ecb62995f68bb549"
+  instance_type          = "t3.micro"
+  tags = {
+    Name = " uttej-instance"
+  }
+}
