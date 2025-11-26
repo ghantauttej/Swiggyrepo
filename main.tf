5a09a1e4c05c95641970eb0bb8b089a5ec9c6f26
Uttej Ghanta <uttejmulticloud@gmail.com>
2025-11-26 10:44:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,11 @@ provider "aws" {
 
 
 resource "aws_instance" "one" {
+ count                  = 2
  ami                    = "ami-0ecb62995f68bb549"
   instance_type          = "t3.micro"
   tags = {
     Name = " uttej-instance"
   }
 }
+
