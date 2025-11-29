d4e04c8d7ec5cd274257b921f07bd28b65ac4501
Uttej Ghanta <uttejmulticloud@gmail.com>
2025-11-29 11:00:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "one" {
-  ami           = "ami-0ecb62995f68bb549"
-  instance_type = "t3.micro"
+#### RDS ####
+resource "aws_db_subnet_group" "swiggy-db-sub-grp" {
+  name       = "swiggy-db-sub-grp"
+  subnet_ids = ["${aws_subnet.swiggy-pvt-sub-3.id}","${aws_subnet.swiggy-pvt-sub-4.id}"]
+}
 
-  tags = {
-    Name = "Prod"
-    Env  = "Prod"  # must match Sentinel
+resource "aws_db_instance" "swiggy-db" {
+  allocated_storage           = 100
+  storage_type                = "gp3"
+  engine                      = "mysql"
+  engine_version              = "8.0.43"
+  instance_class              = "db.t4g.micro"
+  identifier                  = "swiggy-db"
+  username                    = "admin"
+  password                    = "Devopsbyraham007*"
+  parameter_group_name        = "default.mysql8.0"
+  db_subnet_group_name        = aws_db_subnet_group.swiggy-db-sub-grp.name
+  vpc_security_group_ids      = ["${aws_security_group.swiggy-db-sg.id}"]
+  multi_az                    = true
+  skip_final_snapshot         = true
+  publicly_accessible          = false
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
   }
 }
+
 
 
 
