fa8f6116643bb36d5c8313cab10be56a635028b7
Uttej Ghanta <uttejmulticloud@gmail.com>
2025-11-29 11:21:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,30 @@ resource "aws_db_instance" "swiggy-db" {
 
 
 
+# Get available AZs
+data "aws_availability_zones" "az" {
+  state = "available"
+}
+
+# Create 2 private subnets for RDS
+resource "aws_subnet" "rds_private" {
+  count                   = 2
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = cidrsubnet(data.aws_vpc.default.cidr_block, 4, count.index + 10)
+  availability_zone       = data.aws_availability_zones.az.names[count.index]
+  map_public_ip_on_launch = false
+}
+
+# Subnet group required for RDS
+resource "aws_db_subnet_group" "swiggy_subnet" {
+  name       = "swiggy-db-subnet-group"
+  subnet_ids = aws_subnet.rds_private[*].id
+
+  tags = {
+    Name = "swiggy-db-subnet-group"
+  }
+}
+
 
 
 
