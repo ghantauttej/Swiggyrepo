9f3bb2afd6e571137973742c2b4c3849dddaaeca
Uttej Ghanta <uttejmulticloud@gmail.com>
2025-12-19 20:44:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "one" {
-  ami           = "ami-0ecb62995f68bb549"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Prod"
-    Env  = "Prod"  # must match Sentinel
-  }
-}
-
+#! /bin/bash
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
+cd /var/www/html
+git clone https://github.com/karishma1521success/swiggy-clone.git
+mv swiggy-clone/* .
+tail -100f /var/log/httpd/access_log
 
 
